3e9e7281de58cd48b32efbd2532f8429a4b69233
Carson Tang <carson@visor.gg>
2018-07-05 09:31:28 -0700

    Add major and minor versioning
    Add major and minor versioning, and print it out in program.

---
+++ include/cptr.h
@@ -0,0 +1,2 @@
+#define cptr_VERSION_MAJOR 0
+#define cptr_VERSION_MINOR 1
--- src/cptr.c
+++ src/cptr.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "cptr.h"
 
 int main(int argc, char *argv[])
 {
+				fprintf(stdout, "Version %d.%d\n", cptr_VERSION_MAJOR, cptr_VERSION_MINOR);
 				printf("Hello, CMake World!\n");
 				return 0;
 }
